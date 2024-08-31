5e0488493467ab9e885df8542153f026c3c060c6
Aaron Peterham <47309189+amp7368@users.noreply.github.com>
2024-08-30 22:31:41 -0400

    Update to using a service account rather than using Auth 2.0 Client IDs

--- src/main/java/com/ambrosia/backup/google/GoogleService.java
+++ src/main/java/com/ambrosia/backup/google/GoogleService.java
@@ -3,29 +3,23 @@ package com.ambrosia.backup.google;
 import com.ambrosia.backup.Ambrosia;
 import com.ambrosia.backup.config.AmbrosiaDatabaseConfig;
 import com.ambrosia.backup.sql.TableName;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.Sheets.Builder;
 import com.google.api.services.sheets.v4.Sheets.Spreadsheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.Sheet;
 import com.google.api.services.sheets.v4.model.SheetProperties;
 import com.google.api.services.sheets.v4.model.Spreadsheet;
 import com.google.api.services.sheets.v4.model.SpreadsheetProperties;
-import java.io.BufferedReader;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.GeneralSecurityException;
@@ -34,13 +28,7 @@ import java.util.List;
 public class GoogleService {
 
     private static final String APPLICATION_NAME = "Ambrosia Backups";
-    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
-    /**
-     * Global instance of the scopes required by this quickstart. If modifying these scopes, delete your previously saved tokens/
-     * folder.
-     */
     private static final List<String> SCOPES = List.of(SheetsScopes.SPREADSHEETS, DriveScopes.DRIVE_FILE);
     private static final Path CREDENTIALS_FILE_PATH = Ambrosia.getFile("./credentials.json").toPath();
     private static Sheets SHEETS;
@@ -59,35 +47,28 @@ public class GoogleService {
     }
 
     public static Spreadsheets sheets() {
-        return SHEETS.spreadsheets();
+        return sheetsService().spreadsheets();
     }
 
-    private static Credential getCredentials(NetHttpTransport HTTP_TRANSPORT) throws IOException {
-        // Load client secrets.
+    private static HttpCredentialsAdapter getCredentials() throws IOException {
         if (!Files.exists(CREDENTIALS_FILE_PATH)) {
-            throw new FileNotFoundException(
-                "Please include file: %s for Google Credentials".formatted(CREDENTIALS_FILE_PATH.toAbsolutePath()));
+            String msg = "Please include file: %s for Google Credentials".formatted(CREDENTIALS_FILE_PATH.toAbsolutePath());
+            throw new FileNotFoundException(msg);
         }
-        BufferedReader in = Files.newBufferedReader(CREDENTIALS_FILE_PATH);
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, in);
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-            HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-            .setDataStoreFactory(new FileDataStoreFactory(Ambrosia.getFile(TOKENS_DIRECTORY_PATH)))
-            .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        InputStream in = Files.newInputStream(CREDENTIALS_FILE_PATH);
+        GoogleCredentials clientSecrets = GoogleCredentials.fromStream(in)
+            .createScoped(SCOPES);
+        return new HttpCredentialsAdapter(clientSecrets);
     }
 
     public static void load() throws IOException, GeneralSecurityException {
-        NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Credential credentials = getCredentials(HTTP_TRANSPORT);
-        SHEETS = new Builder(HTTP_TRANSPORT, JSON_FACTORY, credentials)
+        NetHttpTransport SHEETS_HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        GsonFactory gson = GsonFactory.getDefaultInstance();
+        SHEETS = new Sheets.Builder(SHEETS_HTTP_TRANSPORT, gson, getCredentials())
             .setApplicationName(APPLICATION_NAME)
             .build();
         NetHttpTransport DRIVE_HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        DRIVE = new Drive.Builder(DRIVE_HTTP_TRANSPORT, JSON_FACTORY, getCredentials(DRIVE_HTTP_TRANSPORT))
+        DRIVE = new Drive.Builder(DRIVE_HTTP_TRANSPORT, gson, getCredentials())
             .setApplicationName(APPLICATION_NAME)
             .build();
     }
